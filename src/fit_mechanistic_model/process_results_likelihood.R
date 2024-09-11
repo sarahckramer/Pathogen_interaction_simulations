70f460910d3f599f1889648ed32654a0a5ac331f
Sarah Kramer <kramer@mpiib-berlin.mpg.de>
2024-09-11 14:46:30 +0200

    run round 3

--- src/fit_mechanistic_model/process_results_likelihood.R
+++ src/fit_mechanistic_model/process_results_likelihood.R
@@ -169,6 +169,9 @@ rm(int_set, i)
 res <- bind_rows(res_LIST)
 rm(res_LIST, res_dir)
 
+# STORE RESULTS:
+res1 <- res
+
 # Set any unrealistic values to NA:
 res$delta1[res$delta1 > 7.0] <- NA
 res$delta2[res$delta2 > 7.0] <- NA
@@ -315,15 +318,205 @@ ci_start <- ci_start %>%
          w_delta_i_10 = if_else(minmax == 'max' & w_delta_i_10 > 0.5, 0.5, w_delta_i_10))
 
 # Write start ranges to file:
-write_rds(ci_start, file = 'results/trajectory_matching/round2CI_startvals.rds')
+# write_rds(ci_start, file = 'results/trajectory_matching/round2CI_startvals.rds')
 rm(ci_start)
 
 # ---------------------------------------------------------------------------------------------------------------------
 
-# TEMPORARY: Check round 1 accuracy
+# Load and process results (round 2)
+
+# Read in results:
+res_dir <- 'results/trajectory_matching/round2/'
+
+# Loop through interaction parameter sets:
+res_LIST = vector('list', length = 16)
+for (int_set in 1:16) {
+  print(int_set)
+  
+  res_list_TEMP = vector('list', length = length(ids_to_fit))
+  
+  # Loop through synthetic datasets:
+  for (i in seq_along(ids_to_fit)) {
+    data_id <- ids_to_fit[i]
+    pat_temp <- paste0('_', int_set, '_', data_id, '_')
+    
+    print(data_id)
+    
+    res_list_TEMP[[i]] <- load_and_format_results(list.files(path = res_dir, pattern = pat_temp, full.names = TRUE), get_top_5_perc = TRUE) %>%
+      mutate(int_set = int_set, .id = data_id, .before = 'Ri1')
+    rm(data_id, pat_temp)
+    
+  }
+  
+  res_LIST[[int_set]] <- bind_rows(res_list_TEMP)
+  rm(res_list_TEMP)
+}
+rm(int_set, i)
+# only one: 6 (90), 12 (46), 13 (90), 14 (46, 13)
+# less than 5: 4 (46), 6 (96), 7 (13), 8 (13), 10 (90, 96, 28, 13), 11 (90), 12 (90, 28, 13), 13 (28, 46, 13), 15 (90, 13) 
+
+# Compile all results:
+res <- bind_rows(res_LIST)
+rm(res_LIST, res_dir)
+
+# STORE RESULTS:
+res2 <- res
+
+# Set any unrealistic values to NA:
+res$delta1[res$delta1 > 7.0] <- NA
+res$delta2[res$delta2 > 7.0] <- NA
+
+res$rho1[res$rho1 == 1.0] <- NA
+res$rho2[res$rho2 == 1.0] <- NA
+
+# res <- res %>%
+#   mutate(across(contains('w_delta'), ~ if_else(.x == 1.0, NA, .x)))
+
+# Since phi=0 is equivalent to phi=52.25, don't use full range; transform so that we can select from only best-supported range:
+phi_ranges_orig <- res %>%
+  group_by(int_set, .id) %>%
+  summarise(min1 = min(phi1), max1 = max(phi1), min2 = min(phi2), max2 = max(phi2)) %>%
+  mutate(range1 = max1 - min1, range2 = max2 - min2) %>%
+  select(int_set:.id, range1:range2)
+
+phi_ranges_trans <- res %>%
+  mutate(phi1 = if_else(phi1 < 1, phi1 + 52.25, phi1),
+         phi2 = if_else(phi2 < 1, phi2 + 52.25, phi2)) %>%
+  group_by(int_set, .id) %>%
+  summarise(min1 = min(phi1), max1 = max(phi1), min2 = min(phi2), max2 = max(phi2)) %>%
+  mutate(range1 = max1 - min1, range2 = max2 - min2) %>%
+  select(int_set:.id, range1:range2)
+
+range_reduct <- c(phi_ranges_orig %>%
+                    inner_join(phi_ranges_trans, by = c('int_set', '.id')) %>%
+                    filter(range1.y < range1.x) %>%
+                    nrow(),
+                  phi_ranges_orig %>%
+                    inner_join(phi_ranges_trans, by = c('int_set', '.id')) %>%
+                    filter(range2.y < range2.x) %>%
+                    nrow())
+
+if (any(range_reduct > 0)) {
+  res <- res %>%
+    mutate(phi1 = if_else(phi1 < 1, phi1 + 52.25, phi1),
+           phi2 = if_else(phi2 < 1, phi2 + 52.25, phi2))
+}
+
+rm(phi_ranges_orig, phi_ranges_trans, range_reduct)
+
+# ---------------------------------------------------------------------------------------------------------------------
+
+# Get start ranges for round 3
+
+# Drop column for log-likelihoods:
+res <- res %>%
+  select(-loglik)
+
+# List results by int_set/.id:
+res_list <- res %>% group_split(int_set, .id)
+
+# Get minimum and maximum start values:
+res_list <- lapply(res_list, function(ix) {
+  as.data.frame(rbind(summarise(ix %>% group_by(int_set, .id), across(.cols = everything(), \(x) min(x, na.rm = TRUE))),
+                      summarise(ix %>% group_by(int_set, .id), across(.cols = everything(), \(x) max(x, na.rm = TRUE)))))
+})
+
+# Any parameters where minimum and maximum are equal?:
+lapply(res_list, function(ix) {
+  
+  no_range <- c()
+  for (i in 3:ncol(ix)) {
+    if (identical(ix[1, i], ix[2, i])) {
+      no_range <- c(no_range, i)
+    }
+  }
+  
+  return(length(no_range))
+  
+}) %>%
+  unlist() %>%
+  any(. > 0)
+
+# Reset initial conditions so that sum cannot be >1:
+ci_start <- lapply(res_list, function(ix) {
+  
+  # init_cond_estpars <- c('E01', 'E02', 'R01', 'R02', 'R012')
+  
+  ix %>%
+    mutate(minmax = c('min', 'max')) %>%
+    # select(int_set, .id, contains(init_cond_estpars), minmax) %>%
+    mutate(sum = E01 + E02 + R01 + R02 + R012)
+  
+}) %>%
+  bind_rows()
+rm(res_list)
+
+expect_true(ci_start %>%
+              filter(minmax == 'min', sum > 1.0) %>%
+              nrow() == 0)
+
+ci_start <- ci_start %>%
+  filter(minmax == 'max', sum > 1.0) %>%
+  group_by(int_set, .id) %>%
+  mutate(across(c('E01', 'E02', 'R01', 'R02', 'R012'),
+                ~ .x - ((sum - 0.9999999) * (.x / sum)))) %>%
+  bind_rows(ci_start %>%
+              filter(minmax == 'min')) %>%
+  mutate(minmax = factor(minmax, levels = c('min', 'max'))) %>%
+  arrange(minmax, .by_group = TRUE)
+
+# Ensure upper bounds still greater than lower:
+expect_true(
+  ci_start %>%
+    filter(minmax == 'max') %>%
+    select(int_set:.id, E01:R012) %>%
+    inner_join(ci_start %>%
+                 filter(minmax == 'min') %>%
+                 select(int_set:.id, E01:R012),
+               by = c('int_set', '.id')) %>%
+    mutate(E01 = E01.x > E01.y,
+           E02 = E02.x > E02.y,
+           R01 = R01.x > R01.y,
+           R02 = R02.x > R02.y,
+           R012 = R012.x > R012.y) %>%
+    ungroup() %>%
+    select(E01:R012) %>%
+    all(TRUE)
+)
+
+# Ensure that upper bounds now sum to <1:
+expect_equal(ci_start %>%
+               mutate(sum = E01 + E02 + R01 + R02 + R012) %>%
+               filter(sum > 1.0) %>%
+               nrow(), 0)
+
+# Remove 'sum' column:
+ci_start <- ci_start %>%
+  select(!sum)
+
+# If w_delta_i range greater than c(0, 0.5), reset:
+ci_start <- ci_start %>%
+  mutate(w_delta_i_1 = if_else(minmax == 'max' & w_delta_i_1 > 0.5, 0.5, w_delta_i_1),
+         w_delta_i_2 = if_else(minmax == 'max' & w_delta_i_2 > 0.5, 0.5, w_delta_i_2),
+         w_delta_i_3 = if_else(minmax == 'max' & w_delta_i_3 > 0.5, 0.5, w_delta_i_3),
+         w_delta_i_4 = if_else(minmax == 'max' & w_delta_i_4 > 0.5, 0.5, w_delta_i_4),
+         w_delta_i_5 = if_else(minmax == 'max' & w_delta_i_5 > 0.5, 0.5, w_delta_i_5),
+         w_delta_i_6 = if_else(minmax == 'max' & w_delta_i_6 > 0.5, 0.5, w_delta_i_6),
+         w_delta_i_7 = if_else(minmax == 'max' & w_delta_i_7 > 0.5, 0.5, w_delta_i_7),
+         w_delta_i_8 = if_else(minmax == 'max' & w_delta_i_8 > 0.5, 0.5, w_delta_i_8),
+         w_delta_i_9 = if_else(minmax == 'max' & w_delta_i_9 > 0.5, 0.5, w_delta_i_9),
+         w_delta_i_10 = if_else(minmax == 'max' & w_delta_i_10 > 0.5, 0.5, w_delta_i_10))
+
+# Write start ranges to file:
+# write_rds(ci_start, file = 'results/trajectory_matching/round3CI_startvals.rds')
+rm(ci_start)
+
+# ---------------------------------------------------------------------------------------------------------------------
+
+# TEMPORARY: Check round 2 accuracy
 
 # Join information on true parameter values:
-res <- res %>%
+res <- res2 %>%
   pivot_longer(Ri1:w_delta_i_10, names_to = 'param') %>%
   inner_join(true_params %>% pivot_longer(Ri1:w_delta_i_10, names_to = 'param', values_to = 'truth'),
              by = c('int_set', '.id', 'param'))
